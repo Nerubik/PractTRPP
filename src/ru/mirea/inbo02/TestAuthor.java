e9003ba298d1c0aba7eb6af4fc2eb86c5e530647
4kul4 <maletskyamargarita@gmail.com>
2020-09-14 13:48:51 +0300

    Initial commit

---
+++ src/ru/mirea/inbo02/TestAuthor.java
@@ -0,0 +1,9 @@
+package ru.mirea.inbo02;
+
+public class TestAuthor {
+    public static void main(String[] args){
+        Author a1 = new Author("Paul", "paulpy@gmail.com", 'M');
+        System.out.println(a1);
+    }
+
+}
